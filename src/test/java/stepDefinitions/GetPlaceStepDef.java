1031453c863855c842bdcb823d5b17e8f3a529ca
prachi <prachi.gupta0403@gmail.com>
2024-03-08 13:24:08 +0530

    commented duplicate code

--- src/test/java/stepDefinitions/GetPlaceStepDef.java
+++ src/test/java/stepDefinitions/GetPlaceStepDef.java
@@ -17,11 +17,11 @@ public class GetPlaceStepDef extends utils {
 
     }
 
-    @Given("^When user hits API with (.+) of Add Place API$")
-    public void when_user_hits_api_with_place_id_(String place_id) throws IOException {
-        // Write code here that turns the phrase above into concrete actions
-        System.out.println("Place id is "+place_id);
-        req= given().spec(requestSpecification()).queryParam("place_id",place_id);
+//    @Given("^When user hits API with (.+) of Add Place API$")
+//    public void when_user_hits_api_with_place_id_(String place_id) throws IOException {
+//        // Write code here that turns the phrase above into concrete actions
+//        System.out.println("Place id is "+place_id);
+//        req= given().spec(requestSpecification()).queryParam("place_id",place_id);
 
-    }
+//    }
 }
